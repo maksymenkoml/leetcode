67c30018540a36826ccbf1bbb835f1483b75b2a5
maksymenko_ml <maksymenko.ml@gmail.com>
2023-01-28 23:15:31 -0500

    add 1672. Richest Customer Wealth

--- easy/1672_richest_customer_wealth_test.go
+++ easy/1672_richest_customer_wealth_test.go
@@ -1,6 +1,7 @@
 package easy
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,8 +13,8 @@ func Test1672(t *testing.T) {
 		want     int
 	}
 	s := []examples{
-		{accounts: [][]int{{1, 2, 3}, {3, 2, 1}}, want: 6},
 		{accounts: [][]int{{1, 5}, {7, 3}, {3, 5}}, want: 10},
+		{accounts: [][]int{{1, 2, 3}, {3, 2, 1}}, want: 6},
 		{accounts: [][]int{{2, 8, 7}, {7, 1, 3}, {1, 9, 5}}, want: 17},
 	}
 	for _, tst := range s {
@@ -24,6 +25,59 @@ func Test1672(t *testing.T) {
 }
 
 func maximumWealth(accounts [][]int) int {
+	wg := sync.WaitGroup{}
+
+	wealths := make(chan int, len(accounts))
+	for _, account := range accounts {
+		wg.Add(1)
+		go func(account []int) {
+			defer wg.Done()
+			var wealth int
+			for _, balance := range account {
+				wealth += balance
+			}
+			wealths <- wealth
+		}(account)
+	}
+
+	wg.Wait()
+
+	var max int
+	for len(wealths) > 0 {
+		w := <-wealths
+		if w > max {
+			max = w
+		}
+	}
+
+	return max
+}
+
+func maximumWealth2(accounts [][]int) int {
+	wealths := make(chan int)
+
+	for _, account := range accounts {
+		go func(account []int) {
+			var wealth int
+			for _, balance := range account {
+				wealth += balance
+			}
+			wealths <- wealth
+		}(account)
+	}
+
+	var max int
+	for i := 0; i < len(accounts); i++ {
+		w := <-wealths
+		if w > max {
+			max = w
+		}
+	}
+
+	return max
+}
+
+func maximumWealth1(accounts [][]int) int {
 	customersWealth := make([]int, len(accounts))
 
 	var max int
